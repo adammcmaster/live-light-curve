7589a8ec07c861d4dfd012e329b360f955bc5fb1
Adam McMaster <adam.mcmaster@open.ac.uk>
2022-07-07 14:33:53 +0100

    Plot real data

--- plot.py
+++ plot.py
@@ -6,29 +6,20 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 
-DISPLAY_DURATION = 3000
-MIN_FLUX = 0
-MAX_FLUX = 100
+from sensor_serial import read_flux
+
+DISPLAY_DURATION = 300
 
 
 fig, ax = plt.subplots()
+plt.axis("off")
 (line,) = ax.plot([], [], lw=2)
 ax.grid()
 xdata, ydata = list(range(DISPLAY_DURATION)), [0 for x in range(DISPLAY_DURATION)]
 
 
-def data_gen():
-    newflux = ydata[-1] + (random.random() * random.choice([1, -1]))
-    if newflux < MIN_FLUX:
-        yield MIN_FLUX
-    elif newflux > MAX_FLUX:
-        yield MAX_FLUX
-    else:
-        yield newflux
-
-
 def init():
-    ax.set_ylim(MIN_FLUX, MAX_FLUX)
+    ax.set_ylim(min(ydata), max(ydata))
     ax.set_xlim(0, DISPLAY_DURATION)
     line.set_data(xdata, ydata)
     return (line,)
@@ -37,11 +28,11 @@ def init():
 def run(newflux):
     ydata.append(newflux)
     del ydata[0]
-    xmin, xmax = ax.get_xlim()
+    ax.set_ylim(min(ydata) * 0.8, max(ydata) * 1.2)
     line.set_data(xdata, ydata)
 
     return (line,)
 
 
-ani = animation.FuncAnimation(fig, run, data_gen, interval=1, init_func=init)
+ani = animation.FuncAnimation(fig, run, read_flux, interval=1, init_func=init)
 plt.show()
