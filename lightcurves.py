2cbcc4d4cdedebafb8d327e5815adacc71391ee6
Adam McMaster <adam.mcmaster@open.ac.uk>
2022-08-17 17:45:13 +0100

    Tweak curves

--- lightcurves.py
+++ lightcurves.py
@@ -6,39 +6,41 @@ FPS = 25
 DELAY = 1 / FPS
 TIMESCALE = 10
 MIN_BRIGHTNESS = 0.1
-MAX_BRIGHTNESS = 1.0
+MAX_BRIGHTNESS = 0.9
 
 
 class LightcurveGenerator(object):
-    PULSATOR = (
-        (0.5, 0),
-        (0.4, 1),
-        (1, 0),
-        # (0.2, 0),
+    ROTATOR = (
+        (1.5, 0.3),
+        (1.5, 0.7),
     )
-    ECLIPSING = (
-        (0.7, 1),
-        (0.6, 1),
+    EXOPLANET = (
+        (1, 1),
         (0.8, 0.5),
         (0.3, 0.5),
+        (0.8, 1),
     )
     LENSING = (
-        (0.5, 0),
-        (0.6, 0.9),
-        (0.7, 0),
-        #   (0.2, 0),
+        (1, 0),
+        (0.6, 1),
+        (0.6, 0),
     )
 
     @classmethod
     def random(cls):
         while True:
-            gen = cls(random.choice((cls.PULSATOR, cls.ECLIPSING, cls.LENSING)))
-            print(gen.lc_type)
+            gen = cls(random.choice((cls.ROTATOR, cls.EXOPLANET, cls.LENSING)))
             for val in gen.repeat():
                 yield val
 
     def __init__(
-        self, lc_type, fps=25, timescale=10, min_brightness=0.1, max_brightness=1.0
+        self,
+        lc_type,
+        init_value=0,
+        fps=25,
+        timescale=10,
+        min_brightness=0.1,
+        max_brightness=1.0,
     ):
         self.lc_type = lc_type
         self.fps = fps
@@ -46,21 +48,8 @@ class LightcurveGenerator(object):
         self.timescale = timescale
         self.max_brightness = max_brightness
         self.min_brightness = min_brightness
-        self.value = 0
-
-    def fade(self, dur, target):
-        num_frames = int(dur * self.timescale * self.fps)
-        if num_frames == 0:
-            num_frames = 1
-        step_size = (target - self.value) / num_frames
-        for i in range(num_frames):
-            new_value = self.value + step_size
-            if new_value < self.min_brightness:
-                new_value = self.min_brightness
-            if new_value > self.max_brightness:
-                new_value = self.max_brightness
-            self.value = new_value
-            yield self.value
+        self.value = init_value
+        self.init_value = init_value
 
     def fade_curve(self, dur, target):
         if target > self.max_brightness:
@@ -88,10 +77,11 @@ class LightcurveGenerator(object):
         self.value = target
         yield self.value
 
-    def repeat(self, num_repeats=3):
+    def repeat(self, num_repeats=5):
         for i in range(num_repeats):
             for duration, target in self.lc_type:
                 for val in self.fade_curve(duration, target):
                     yield val
-        for val in self.fade_curve(1, 0):
+
+        for val in self.fade_curve(1, self.init_value):
             yield val
--- plot_simulated.py
+++ plot_simulated.py
@@ -8,7 +8,7 @@ import matplotlib.animation as animation
 
 from lightcurves import LightcurveGenerator
 
-DISPLAY_DURATION = 1000
+DISPLAY_DURATION = 2000
 
 
 fig, ax = plt.subplots()
